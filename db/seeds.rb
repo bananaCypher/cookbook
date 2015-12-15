c2927cb0e764ad2376fb0125bd363863a2965154
Callum Hogg <bananaCypher@gmail.com>
2015-12-15 15:20:18 +0000

    Created the Category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Categories
+Category.delete_all
+Category.create(name: 'Starter', description: 'A small amount of food eaten at the start of a meal before the main part.')
+Category.create(name: 'Main Course', description: 'The main part of a meal.')
+Category.create(name: 'Dessert', description: 'A usually sweet course or dish, as of fruit, ice cream, or pastry, served at the end of a meal.')
