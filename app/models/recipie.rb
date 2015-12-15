25a1b1e4b4912e66d249497d37386b97ce40784b
Callum Hogg <bananaCypher@gmail.com>
2015-12-15 15:39:21 +0000

    Created the Recipie seed

--- app/models/recipie.rb
+++ app/models/recipie.rb
@@ -1,2 +1,3 @@
 class Recipie < ActiveRecord::Base
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,55 @@
 
 # Categories
 Category.delete_all
-Category.create(name: 'Starter', description: 'A small amount of food eaten at the start of a meal before the main part.')
-Category.create(name: 'Main Course', description: 'The main part of a meal.')
-Category.create(name: 'Dessert', description: 'A usually sweet course or dish, as of fruit, ice cream, or pastry, served at the end of a meal.')
+starter = Category.create(name: 'Starter', description: 'A small amount of food eaten at the start of a meal before the main part.')
+main_course = Category.create(name: 'Main Course', description: 'The main part of a meal.')
+dessert = Category.create(name: 'Dessert', description: 'A usually sweet course or dish, as of fruit, ice cream, or pastry, served at the end of a meal.')
+
+# Recipies
+Recipie.delete_all
+soup = Recipie.new(name: 'Lentil Soup', description: 'Lentil soup is comforting, simple, and easy to throw together with the bare minimum of ingredients.', instructions: "
+  1 tablespoon olive oil\n
+  1 large carrot, diced\n
+  2 large ribs celery, diced\n
+  1 small yellow onion, diced\n
+  1/4 teaspoon salt, plus additional to taste\n
+  1 cup red lentils\n
+  4 cups water or low-sodium broth\n
+  1 whole bay leaf\n
+  2 tablespoons lemon juice from 1/2 large lemon\n
+  Olive oil, yogurt, or other topping, to serve\n
+  \n
+  In a medium (2- to 3-quart) saucepan or Dutch oven, heat the olive oil over medium heat. Add the carrot, celery, onion, and 1/4 teaspoon salt, stir to combine, then cover and let the vegetables sweat until the onions are soft and translucent, about 5 minutes.\n
+  \n
+  Add the lentils, water or broth, and bay leaf. Bring up to a boil, then turn the heat down to low and let simmer, covered, until the lentils begin to fall apart, about 20 minutes.\n
+  \n
+  Turn off the heat, stir in the lemon juice, and add salt to taste. Ladle into bowls and serve. Leftovers will keep refrigerated for 5 days.\n
+  ")
+soup.category = starter
+soup.save
+
+beef = Recipie.new(name: 'Roast Beef', description: 'Roast beef is a signature national dish of England and holds cultural meaning for the English.', instructions: "
+  To prepare your beef:\n
+  Take your beef out of the fridge 30 minutes before it goes into the oven. Preheat your oven to 240°C/475°F/ gas 9. There’s no need to peel the vegetables – just give them a wash and roughly chop them. Break the garlic bulb into cloves, leaving them unpeeled.\n
+  \n
+  Pile all the veg, garlic and herbs into the middle of a large roasting tray and drizzle with olive oil. Drizzle the beef with olive oil and season well with salt and pepper, rubbing it all over the meat. Place the beef on top of the vegetables.\n
+  \n
+  To cook your beef:\n
+  Place the roasting tray in the preheated oven. Turn the heat down immediately to 200°C/400°F/gas 6 and cook for 1 hour for medium beef. If you prefer it medium-rare, take it out 5 to 10 minutes earlier. For well done, leave it in for another 10 to 15 minutes.\n
+  \n
+  If you’re doing roast potatoes and veggies, this is the time to crack on with them – get them into the oven for the last 45 minutes of cooking. Baste the beef halfway through cooking and if the veg look dry, add a splash of water to the tray to stop them burning.\n
+  \n
+  When the beef is cooked to your liking, take the tray out of the oven and transfer the beef to a board to rest for 15 minutes or so. Cover it with a layer of tinfoil and a tea towel and put aside while you make your gravy, horseradish sauce and Yorkshire puddings.")
+beef.category = main_course
+beef.save
+
+banofee_pie = Recipie.new(name: 'Banofee Pie', description: 'Arguably the best dessert in the world!.', instructions: "
+  melt butter and add crushed biscuits, spread on bottom of tin & chill. boil the tins of condensed milk for 2-2.5 hours. BE SURE THAT THE TINS ARE SUBMERGED IN WATER AT ALL TIMES - OR ELSE YOU WILL BE CLEANING THE CEILING!!!\n
+  \n
+  when cool spread the toffee over the biscuit base. slice up a couple of ripe bananas on top of toffee whip up the cream and spread over the banana.\n
+  \n
+  grate some chocolate or even chocolate curls, or drizzle melted chocolate over the top chill (or serve straight away) and serve to very happy people!\n
+  \n
+  BIG TIP one can boil up a load of tins of condensed milk. once they are cooked they will keep very safely for months in the larder or cupboard. just keep an eye on water levels. this makes it the fastest dessert in town if one has the toffee already. also the biscuit base mix freezes very well.")
+banofee_pie.category = dessert
+banofee_pie.save
