0e88c4e7bf3e63d218188beb67eff600171e9797
Callum Hogg <bananaCypher@gmail.com>
2016-01-04 14:01:58 +0000

    Added validation to both Categories and Recipes models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: {maximum: 255}
+  validates :description, length: {maximum: 1024}
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :ingredients
+
+  # Validators
+  validates :name, presence: true
+  validates :name, length: {maximum: 255}
+  validates :description, length: {maximum: 1024}
+  validates :instructions, length: {maximum: 1024}
+  validates :category, presence: true
 end
